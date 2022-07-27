30c961304a741d94eb54006e9b434fa5f96fa9a6
jonata <manojhonson181@gmail.com>
2022-07-27 12:02:28 -0300

    Configurado projeto spring boot

---
+++ src/main/java/github/jonataazevedo/gestor/api/GestorApiApplication.java
@@ -0,0 +1,13 @@
+package github.jonataazevedo.gestor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/jonataazevedo/gestor/api/GestorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package github.jonataazevedo.gestor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
